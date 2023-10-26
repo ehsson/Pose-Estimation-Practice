01703023ce88d7142e8c73068f8150c325e226a8
ehsson <ehsson@naver.com>
2023-10-27 00:39:59 +0900

    modify dataset.py

--- dataset.py
+++ dataset.py
@@ -20,8 +20,10 @@ def generate_gaussian(t, x, y, sigma=10):
     h,w = t.shape
 
     # Heatmap pixel per output pixel
-    mu_x = int(0.5 * (x + 1.) * w)
-    mu_y = int(0.5 * (y + 1.) * h)
+    # mu_x = int(0.5 * (x + 1.) * w)
+    # mu_y = int(0.5 * (y + 1.) * h)
+    mu_x = x
+    mu_y = y
 
     tmp_size = sigma * 3
 
@@ -37,9 +39,9 @@ def generate_gaussian(t, x, y, sigma=10):
     tx = np.arange(0, size, 1, np.float32)
     ty = tx[:, np.newaxis]
     x0 = y0 = size // 2
-
+    _gaussians = {}
     # The gaussian is not normalized, we want the center value to equal 1
-    g = _gaussians[sigma] if sigma in _gaussians else tensor(np.exp(- ((tx - x0) ** 2 + (ty - y0) ** 2) / (2 * sigma ** 2)))
+    g = _gaussians[sigma] if sigma in _gaussians else torch.Tensor(np.exp(- ((tx - x0) ** 2 + (ty - y0) ** 2) / (2 * sigma ** 2)))
     _gaussians[sigma] = g
 
     # Determine the bounds of the source gaussian
@@ -50,8 +52,7 @@ def generate_gaussian(t, x, y, sigma=10):
     img_x_min, img_x_max = max(0, x1), min(x2, w)
     img_y_min, img_y_max = max(0, y1), min(y2, h)
 
-    t[img_y_min:img_y_max, img_x_min:img_x_max] = \
-      g[g_y_min:g_y_max, g_x_min:g_x_max]
+    t[img_y_min:img_y_max, img_x_min:img_x_max] = g[g_y_min:g_y_max, g_x_min:g_x_max]
 
     return t
 
@@ -78,14 +79,25 @@ class MPIIDataset(Dataset):
                     self.joints_list.append(json_data[i]['joint_self'])
         
         for img_idx in range(len(self.image_name_list)):
-            image = cv2.imread(image_dir + self.image_name_list[img_idx])
+            image = cv2.imread(image_dir + self.image_name_list[img_idx], 0)
+            image = np.float32(image)
             image = image / 255
             
+            temp_heatmap = []
             joint_points = self.joints_list[img_idx]
             for i in range(len(joint_points)):
-                heatmap = 
+                heatmap = generate_gaussian(np.zeros((image.shape[0], image.shape[1])), joint_points[i][0], joint_points[i][1])
+                temp_heatmap.append(heatmap)
             
+            temp_heatmap = np.array(temp_heatmap, dtype=np.float32)
+            heatmap = np.max(temp_heatmap, axis=0)
             
+            self.image_list.append(image)
+            self.heatmap_list.append(heatmap)
+                
+        print(len(self.image_name_list))
+        print(len(self.image_list))
+        print(len(self.heatmap_list))
         assert len(self.image_list) == len(self.heatmap_list)
             
 
